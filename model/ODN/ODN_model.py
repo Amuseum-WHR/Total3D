6ba7cdac4fdba94a2d21f8b3713c46e548338545
Dong Wentao <1134136579@qq.com>
2022-11-23 20:58:25 +0800

    finish build the Module

--- model/ODN/ODN_model.py
+++ model/ODN/ODN_model.py
@@ -1,4 +1,98 @@
+'''
+Objection Detection Net
+Based on Total3DUnderstanding by ynie
+'''
+
 import torch
 import torch.nn as nn
 import model.utils.resnet as resnet
+import torch.utils.model_zoo as model_zoo
+from model.ODN.Relation_Net import RelationNet
+from configs.data_config import NYU40CLASSES
+class ODN(nn.Module):
+    def __init__(self, cfg):
+        super(ODN, self).__init__()
+        bin = cfg.dataset_config.bins
+        self.ORI_BIN_NUM = len(bin['ori_bin'])
+        self.CENTER_BIN_NUM = len(bin['centroid_bin'])
 
+        self.resnet = resnet.resnet34(pretrained = False)
+
+        self.relnet = RelationNet()
+
+        Linear_input_size = 2048 + len(NYU40CLASSES) # Depend on the dataset
+
+        # For Projection offset (2D)
+        self.fc1 = nn.Linear(Linear_input_size, 128)
+        self.fc2 = nn.Linear(128, 2)
+        
+        # For distance to Camara Center, which is used to calculate Centroid of 3D Box 
+        self.fc3 = nn.Linear(Linear_input_size, 128)
+        self.fc4 = nn.Linear(128, 2 * self.CENTER_BIN_NUM)
+
+        # For the size of 3D Box
+        self.fc5 = nn.Linear(Linear_input_size, 128)
+        self.fc6 = nn.Linear(128, 3)
+
+        # For orientation of 3D box (1 dimension)
+        self.fc7 = nn.Linear(Linear_input_size, 128)
+        self.fc8 = nn.Linear(128, 2 * self.ORI_BIN_NUM)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p = 0.5)
+        # Load Pretrained ResNet Model
+        pretrained_dict = model_zoo.load_url(resnet.model_urls['resnet34'])
+        model_dict = self.resnet.state_dict()
+        pretrained_dict = {k: v for k,v in pretrained_dict.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        self.resnet.load_state_dict(model_dict)
+
+        # initialize weights (I don't understand why it is necessary)
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                if hasattr(m.bias, 'data'):
+                    m.bias.data.zero_()
+    def forward(self, x, Geometry_features, split, pair_counts, target):
+        '''
+            x: Patch x Channel x Hight x Width
+            Geometry_feature: sum of pairs x 64
+            split: e.g. [[0,5],[5,8]]
+            pair_counts: e.g. [0,49,113]
+            target: Patch x num of class: target[i]: object i should belongs to class[i], where targets[i][class[i]] =1 (?)
+        '''
+        a_featrues = self.resnet(x)
+        a_features = a_featrues.view(a_featrues.size(0),-1) # N x 2048
+        r_features = self.relnet(a_featrues, Geometry_features, split, pair_counts) # N x 2048
+        
+        a_r_featrues = torch.add(a_features, r_features) # N x 2048
+        a_r_featrues = torch.cat([a_r_featrues, target], dim = 1) # N x (2048 + class)
+
+        # Use Fc to predict all we want
+        offset = self.fc1(a_r_featrues)
+        offset = self.relu(offset)
+        offset = self.dropout(offset)
+        offset = self.fc2(a_r_featrues)
+
+        distance = self.fc3(a_r_featrues)
+        distance = self.relu(distance)
+        distance = self.dropout(distance)
+        distance = self.fc4(a_r_featrues)
+        distance = distance.view(-1, self.CENTER_BIN_NUM, 2)
+        distance_cls = distance[:,:,0]
+        distance_reg = distance[:,:,0]
+
+        size = self.fc4(a_r_featrues)
+        size = self.relu(size)
+        size = self.dropout(size)
+        size = self.fc6(size)
+
+        orientation = self.fc7(a_r_featrues)
+        orientation = self.relu(orientation)
+        orientation = self.dropout(orientation)
+        orientation = self.fc8(orientation)
+        orientation = orientation.view(-1, self.ORI_BIN_NUM, 2)
+        orientation_cls = orientation[:,:,0]
+        orientation_reg = orientation[:,:,1]
+
+        return size, orientation_reg, orientation_cls, distance_reg, distance_cls, offset
---
+++ model/ODN/Relation_Net.py
@@ -0,0 +1,77 @@
+# Based on RelationNet's implement by ynie in Total3DUnderstanding
+
+import torch
+import torch.nn as nn
+from configs.data_config import Relation_Config
+
+rel_cfg = Relation_Config()
+class RelationNet(nn.Module):
+    # In this implement, ynie use the original appearance feature to replace the V_feature
+    def __init__(self):
+        super(RelationNet, self).__init__()
+        self.df = 2048
+        self.dg = rel_cfg.d_g
+        self.dk = rel_cfg.d_k
+        self.Nr = rel_cfg.Nr
+        self.K = nn.Linear(self.df, self.dk * self.Nr)
+        self.Q = nn.Linear(self.df, self.dk * self.Nr)
+        self.G = nn.Linear(self.dg, 1 * self.Nr)
+        # self.threshold = nn.ReLU()
+        self.threshold = nn.Threshold(1e-6, 1e-6) # In paper Relation Networks, it use Relu
+        self.softmax = nn.Softmax(dim = 1)
+        self.scale_layer = nn.Cov1d(1,1,1)
+
+    def forward(self, a_featrue, g_feature, split, pair_counts):
+        '''
+        a_feature: PatchSize (sum of Several N_i) x 2048, appearance feature
+        g_feature: Number of pairs(sum of several N_i^2) x 64, geometric feature
+        split: Shows how N_i construct the whole patch, e.g. [[0,5],[5,8]] means N_0 = 5, N_1 = 3
+        pair_counts: Shows which image(?) the geometric feature (between a pair) belongs to. eg [0, 49, 113] 0-49(49), 49-113(64)
+        '''
+
+        g_weight = self.G(g_feature)
+        g_weight = self.threshold(g_weight) # N^2 x Nr
+        g_weight.transpose(0,1) # Nr x N^2
+
+        k_featrue = self.K(a_featrue) # N x (Nr x dk)
+        q_feature = self.Q(a_featrue) # N x (Nr x dk)
+
+        k_featrue = k_featrue.view(-1, self.Nr, self.dk) # N x Nr x dk
+        q_feature = q_feature.view(-1, self.Nr, self.dk) # N x Nr x dk
+        
+        k_featrue = k_featrue.transpose(0, 1) # Nr x N x dk
+        q_feature = q_feature.transpose(0, 1) # Nr x N x dk
+
+        v_feature = a_featrue.view(a_featrue.size(0), self.Nr, -1) #N x Nr x (2048/Nr)
+        v_feature = v_feature.transpose(0, 1) # Nr x N x (2048/Nr)
+
+        sqrt_dk = torch.sqrt(torch.tensor(self.dk))
+        
+        r_features = []
+
+        for idx, interval in enumerate(split):
+            sample_k_feature = k_featrue[:, interval[0]:interval[1], :] # N is the number of objects in a single Image Now!
+            sample_q_feature = k_featrue[:, interval[0]:interval[1], :]
+
+            sample_g_weights = g_weight[:, pair_counts[idx]:pair_counts[idx+1]] # Nr x (N_i)^2
+            sample_g_weights = sample_g_weights.view(self.Nr, interval[1]-interval[0], interval[1]-interval[0]) # Nr x N x N
+
+            sample_a_weight = torch.bmm(sample_k_feature, sample_q_feature.transpose(1,2)) # Nr x N x N 
+            sample_a_weight = torch.div(sample_a_weight, sqrt_dk)
+
+            sample_r_weight = self.softmax(torch.log(sample_g_weights) + sample_a_weight) # Nr x N x N 
+
+            sample_v_feature = a_featrue[:,interval[0]:interval[1],:] # Nr x N x 2048/Nr We use the different part of a_feature with different weight.
+
+            sample_r_feature = torch.bmm(sample_v_feature.transpose(1,2) ,sample_r_weight) # Nr x 2048/Nr x N
+
+            sample_r_feature = sample_r_feature.view(-1, sample_r_feature.size(2)) # 2048 x N
+            sample_r_feature = sample_r_feature.transpose(0, 1) # N x 2048
+
+            r_features.append(sample_r_feature)
+
+        r_features = torch.cat(r_features, dim = 0) # (sum N) x 2048
+        r_features = self.scale_layer(r_features.unsqueeze(1)).squeeze(1) #N x 1 x 2048 -> Conv1 -> N x 2048
+
+        return r_features
+    
